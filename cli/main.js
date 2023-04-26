8fa51a0f4b353adbcbe28c1d1925bc9df8566128
paologaleotti <paolo.galeotti@quinck.io>
2023-04-26 15:56:37 +0200

    feat(CLI): :sparkles: implemented CLI entrypoint with commander.js

--- cli/main.js
+++ cli/main.js
@@ -1,28 +1,33 @@
 #!/usr/bin/env node
 
 import chalk from 'chalk'
+import { Command } from 'commander'
 import { handleCreatePackage } from './create-package.js'
 import { handleCreateProject } from './create-project.js'
 import { LIB, MICROSERVICE } from './helper/consts.js'
 
-const [command, secondArgument] = process.argv.slice(2)
+const version = '2.0.0'
+const cli = new Command()
 
-console.log(`${chalk.white.bgBlue(' MULTIJET ')} CLI v1.0.0`)
+cli.name('multijet').description('multijet CLI').version(version)
 
-switch (command) {
-    case 'create':
-        handleCreateProject()
-        break
-    case 'new-microservice':
-        await handleCreatePackage(secondArgument, MICROSERVICE)
-        break
-    case 'new-lib':
-        await handleCreatePackage(secondArgument, LIB)
-        break
-    default:
-        console.log(
-            'Please provide an argument.',
-            chalk.gray('[create, new-microservice, new-lib]'),
-        )
-        break
-}
+cli.command('create')
+    .description('create a new Multijet project')
+    .action(handleCreateProject)
+
+cli.command('new-lib')
+    .description('generate a basic shared lib with given name')
+    .argument('<name>', 'name of the lib to generate')
+    .action(name => {
+        handleCreatePackage(name, LIB)
+    })
+
+cli.command('new-microservice')
+    .description('generate a basic microservice with given name')
+    .argument('<name>', 'name of the service to generate')
+    .action(name => {
+        handleCreatePackage(name, MICROSERVICE)
+    })
+
+console.log(chalk.white.bgBlue(' MULTIJET '))
+cli.parse()
