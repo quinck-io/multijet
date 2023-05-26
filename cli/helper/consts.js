79a48e525d03755cd2d1c62925fbb2f75a689deb
paologaleotti <paolo.galeotti@quinck.io>
2023-05-26 10:10:08 +0200

    feat(CLI): :sparkles: added payments as a optional module

--- cli/helper/consts.js
+++ cli/helper/consts.js
@@ -7,6 +7,7 @@ export const FILE_BLACKLIST = ['.aws-sam', 'node_modules', 'dist', '.turbo']
 export const OPTIONAL_MODULES = [
     { name: 'User authentication', code: 'auth', type: OTHER },
     { name: 'Notifications', code: 'notifications-manager', type: LIB },
+    { name: 'Payments', code: 'payments', type: LIB },
     { name: 'E2E Tests', code: 'e2e', type: OTHER },
     { name: 'DynamoDB utilities', code: 'dynamoose-utils', type: LIB },
     { name: 'Prisma ORM', code: 'prisma', type: OTHER },
