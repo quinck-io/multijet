05f42de2a45f1a3ba4b1663985fcffb8bc858d4c
paologaleotti <paolo.galeotti@quinck.io>
2023-06-05 15:36:04 +0200

    refactor(CLI): :recycle: changed service template to new routes structure

--- cli/templates/microservice/src/app.ts
+++ cli/templates/microservice/src/app.ts
@@ -1,25 +1,15 @@
-import { defaultApp, getHelloWorldRoute } from '@libs/fastify-utils'
+import { defaultApp } from '@libs/fastify-utils'
 import { FastifyInstance, FastifyServerOptions } from 'fastify'
-import { HelloController } from './hello/hello.controller'
+import { createRoutes } from './routes'
 import { AppComponents } from './utils/components'
-
-export function createRoutes(
-    app: FastifyInstance,
-    components: AppComponents,
-): FastifyInstance {
-    const helloController = new HelloController(components.message)
-
-    app.route({ ...getHelloWorldRoute, handler: helloController.getHello })
-
-    return app
-}
 
 export function buildApp(
     components: AppComponents,
     opts?: FastifyServerOptions,
 ): FastifyInstance {
     const app = defaultApp(opts)
-    createRoutes(app, components)
+    const routes = createRoutes(components)
+    routes.forEach(({ config, handler }) => app.route({ ...config, handler }))
 
     return app
 }
