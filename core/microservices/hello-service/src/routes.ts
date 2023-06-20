8ad846904f5ed4acded22babec65d7dc7186e3a9
Stefano Righini <stefano.righini@quinck.io>
2023-06-20 12:13:27 +0200

    feat(core): ⬆️ upgrade @quinck/fastify-openapi-typescript-generator to version 1.2.2

--- core/microservices/hello-service/src/routes.ts
+++ core/microservices/hello-service/src/routes.ts
@@ -1,9 +1,14 @@
-import { RoutesHandlerMapping, getHelloWorld } from '@libs/fastify-utils'
+import {
+    RoutesHandlerMapping,
+    getHelloWorldRouteConfig,
+} from '@libs/fastify-utils'
 import { HelloController } from './hello/hello.controller'
 import { AppComponents } from './utils/components'
 
 export function createRoutes(components: AppComponents): RoutesHandlerMapping {
     const helloController = new HelloController(components.message)
 
-    return [{ config: getHelloWorld, handler: helloController.getHello }]
+    return [
+        { config: getHelloWorldRouteConfig, handler: helloController.getHello },
+    ]
 }
