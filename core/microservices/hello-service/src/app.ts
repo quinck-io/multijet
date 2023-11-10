9b2c607affe5c113fca7eaf14216d4b496c69980
paologaleotti <paolo.galeotti@quinck.io>
2023-11-10 12:18:02 +0100

    feat(core): ✨ added CORS and mappings to microservice template

--- core/microservices/hello-service/src/app.ts
+++ core/microservices/hello-service/src/app.ts
@@ -1,10 +1,11 @@
+import { createApiErrorsLookupService, defaultMappings } from '@libs/api-errors'
 import { defaultApp, diScope } from '@libs/fastify-utils'
 import { FastifyServerOptions } from 'fastify'
 import { appContainer } from './di-container'
 import { createRoutes } from './routes'
 
 export const createApp = (opts?: FastifyServerOptions) => {
-    const app = defaultApp(opts)
+    const app = defaultApp(opts, { cors: true }, createErrorsLookupService())
 
     diScope(app, appContainer(), request => {
         request.services = request.scopedContainer.cradle
@@ -15,4 +16,10 @@ export const createApp = (opts?: FastifyServerOptions) => {
     )
 
     return app
+}
+
+const createErrorsLookupService = () => {
+    const errorsLookupService = createApiErrorsLookupService()
+    errorsLookupService.putMappings(defaultMappings())
+    return errorsLookupService
 }
