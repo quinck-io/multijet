78e014d51c1af27ff3c4418904eb1b3f6acb9309
Stefano Righini <stefano.righini@quinck.io>
2024-05-30 14:22:48 +0200

    feat: ✨ use custom logger in fastify app

--- core/libs/http/src/fastify/app/app.consts.ts
+++ core/libs/http/src/fastify/app/app.consts.ts
@@ -1,7 +1,8 @@
+import { logger } from "@libs/utils"
 import { FastifyServerOptions } from "fastify"
 
 export const DEFAULT_OPTIONS: FastifyServerOptions = {
     ignoreTrailingSlash: true,
     ignoreDuplicateSlashes: true,
-    logger: true,
+    logger,
 }
